a4675c932f2166d3f80dd3affc0b85adb5307867
Lars Francke <lars.francke@gmail.com>
2015-10-06 16:40:30 +0200

    Add Step6.java

---
+++ src/main/java/com/gfk/Step6.java
@@ -0,0 +1,55 @@
+package com.gfk;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellScannable;
+import org.apache.hadoop.hbase.CellScanner;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class Step6 {
+
+  public static void main(String... args) throws IOException {
+    Configuration conf = HBaseConfiguration.create();
+    TableName tableName = TableName.valueOf("gfk");
+    try (Connection connection = ConnectionFactory.createConnection(conf);
+         Table table = connection.getTable(tableName)) {
+      run(table);
+      scanTable(table);
+    }
+  }
+
+  private static void run(Table table) throws IOException {
+    // 1. Delete all versions of the column "stats:reads" in row "1"
+    // 2. Delete the whole row with row key "2"
+  }
+
+  private static void scanTable(Table table) throws IOException {
+    Scan scan = new Scan();
+    try (ResultScanner scanner = table.getScanner(scan)) {
+      for (Result result : scanner) {
+        printResult(result);
+      }
+    }
+  }
+
+  public static void printResult(CellScannable result) throws IOException {
+    CellScanner scanner = result.cellScanner();
+    while (scanner.advance()) {
+      Cell cell = scanner.current();
+      System.out.println("Cell: " + cell + ", Value: " + Bytes.toString(cell.getValueArray(),
+                                                                        cell.getValueOffset(),
+                                                                        cell.getValueLength()));
+    }
+  }
+
+}
