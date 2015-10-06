a0b49e65cbfdda971a264f40f1282774c4c03f8a
Lars Francke <lars.francke@gmail.com>
2015-10-06 16:30:57 +0200

    Solve step4

--- src/main/java/com/gfk/Step4.java
+++ src/main/java/com/gfk/Step4.java
@@ -10,6 +10,10 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -26,8 +30,29 @@ public class Step4 {
 
   private static void run(Table table) throws IOException {
     // 1. Add at least one more row with arbitrary data with row key "2"
+    System.out.println("Putting data...");
+    Put put = new Put(Bytes.toBytes("2"));
+    put.addColumn(Bytes.toBytes("cds"), Bytes.toBytes("delivery_id"), Bytes.toBytes("20000"));
+    put.addColumn(Bytes.toBytes("stats"), Bytes.toBytes("reads"), Bytes.toBytes("456"));
+    table.put(put);
+
     // 2. Scan the table to get all current values and print them
+    System.out.println("Scanning...");
+    Scan scan = new Scan();
+    try (ResultScanner scanner = table.getScanner(scan)) {
+      for (Result result : scanner) {
+        printResult(result);
+      }
+    }
+
     // 3. Scan again but this time also retrieve at least three older versions of each cell
+    System.out.println("Scan with three versions...");
+    scan.setMaxVersions(3);
+    try (ResultScanner scanner = table.getScanner(scan)) {
+      for (Result result : scanner) {
+        printResult(result);
+      }
+    }
   }
 
   public static void printResult(CellScannable result) throws IOException {
