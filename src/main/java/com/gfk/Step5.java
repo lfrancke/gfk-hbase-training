307d55b3e0ab7ba06132b23052fd1ea8ddbfd8de
Lars Francke <lars.francke@gmail.com>
2015-10-06 16:39:53 +0200

    Solve step5

--- src/main/java/com/gfk/Step5.java
+++ src/main/java/com/gfk/Step5.java
@@ -10,6 +10,9 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Increment;
+import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -25,11 +28,41 @@ public class Step5 {
   }
 
   private static void run(Table table) throws IOException {
+    byte[] rowOneBytes = Bytes.toBytes("1");
+    byte[] statsBytes = Bytes.toBytes("stats");
+    byte[] readsBytes = Bytes.toBytes("reads");
+    byte[] writesBytes = Bytes.toBytes("writes");
+
     // 1. Increment our "stats:reads" counter in row "1" by one
+    System.out.println("Incrementing stats:reads...");
+    Increment increment = new Increment(rowOneBytes);
+    increment.addColumn(statsBytes, readsBytes, 1);
+    try {
+      table.increment(increment);
+    } catch (IOException e) {
+      System.out.println("Caught expected IO exception");
+      e.printStackTrace();
+    }
+
     // 2. Increment the non-existing counter "stats:writes" in row "1" by 1 using a different API
+    System.out.println("Incrementing stats:writes...");
+    long counter = table.incrementColumnValue(rowOneBytes, statsBytes, writesBytes, 1);
+    System.out.println(counter);
+
     // 3. Increment the counter "stats:writes" in row "1" by 11
+    System.out.println("Incrementing stats:writes by 11...");
+    counter = table.incrementColumnValue(rowOneBytes, statsBytes, writesBytes, 11);
+    System.out.println(counter);
+
     // 4. Get the counter value for the "stats:reads" column
     // 5. Get the counter value for the "stats:writes" column
+    System.out.println("Getting counter value for stats:reads and stats:writes...");
+    Get get = new Get(rowOneBytes);
+    get.addColumn(statsBytes, readsBytes);
+    get.addColumn(statsBytes, writesBytes);
+    Result result = table.get(get);
+    printResult(result);
+    System.out.println(Bytes.toLong(result.getValue(statsBytes, writesBytes)));
   }
 
   public static void printResult(CellScannable result) throws IOException {
